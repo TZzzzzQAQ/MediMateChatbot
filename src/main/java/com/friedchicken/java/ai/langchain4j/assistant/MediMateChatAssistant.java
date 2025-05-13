9c903b0345022a3f5e543dee35bec80f711460be
TZzzzzQAQ <tzzzqaq@gmail.com>
2025-05-13 22:59:12 +1200

    Finish the tools.

--- src/main/java/com/friedchicken/java/ai/langchain4j/assistant/MediMateChatAssistant.java
+++ src/main/java/com/friedchicken/java/ai/langchain4j/assistant/MediMateChatAssistant.java
@@ -8,7 +8,7 @@ import dev.langchain4j.service.UserMessage;
 import dev.langchain4j.service.spring.AiService;
 import dev.langchain4j.service.spring.AiServiceWiringMode;
 
-@AiService(wiringMode = AiServiceWiringMode.EXPLICIT, chatModel = "openAiChatModel", chatMemoryProvider = "mediMateChatMemoryProvider", tools = "calculatorTools")
+@AiService(wiringMode = AiServiceWiringMode.EXPLICIT, chatModel = "openAiChatModel", chatMemoryProvider = "mediMateChatMemoryProvider", tools = "appointmentTools")
 public interface MediMateChatAssistant {
   @SystemMessage(fromResource = "medimate-chatbot-prompt.txt")
   String chat(@MemoryId ObjectId memoryId, @UserMessage String userMessage);
